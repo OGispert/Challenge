1601ca254067188ab36804df403942e6ab700334
Othmar Gispert <ogisq@allstate.com>
2022-08-25 16:02:33 -0500

    Initial Commit

---
+++ WalmartChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalmartChallenge
+//
+//  Created by ogisq on 8/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
